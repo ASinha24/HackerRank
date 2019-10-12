5372e8825eac2501081a7b41dcd68184874e3263
ASinha24 <alka.sinha2404@gmail.com>
2019-10-12 16:21:41 +0530

    added logic to sort the map and print in formatted way

--- logParser/main.go
+++ logParser/main.go
@@ -3,17 +3,61 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	var (
+		sum     map[string]int
+		domains []string
+		total   int
+		line    int
+	)
+	sum = make(map[string]int)
 	in := bufio.NewScanner(os.Stdin)
 
 	for in.Scan() {
-		fields := strings.Fields(in.Text())
-		fmt.Printf("domain: %s - visits: %s\n", fields[0], fields[1])
+		line++
 
+		fields := strings.Fields(in.Text())
+		if len(fields) != 2 {
+			fmt.Printf("wrong Input %v (line  #%d)\n", fields, line)
+			return
+		}
+
+		domain := fields[0]
+		visit, err := strconv.Atoi(fields[1])
+		if visit < 0 || err != nil {
+			fmt.Printf("wrong Input %v (line  #%d)\n", fields[1], line)
+			return
+		}
+
+		if _, ok := sum[domain]; !ok {
+			domains = append(domains, domain)
+		}
+
+		total += visit
+		sum[domain] += visit
 	}
 
+	fmt.Printf("%-30s %10s\n", "DOMAIN", "VISITS")
+	fmt.Println(strings.Repeat("-", 45))
+
+	sort.Slice(domains, func(i, j int) bool { return domains[i] < domains[j] })
+	for _, domain := range domains {
+		visits := sum[domain]
+		fmt.Printf("%-30s %10d\n", domain, visits)
+	}
+	fmt.Println()
+	fmt.Printf("%-30s %10d\n", "TOTAL", total)
+
+	if err := in.Err(); err != nil {
+		fmt.Println("> err:", err)
+	}
 }
